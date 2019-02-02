e6062405cd620d53911cce08130ffc9d59aa0fea
zhaowei <dang7758@126.com>
2019-02-02 17:34:31 +0800

    a

--- src/main/java/com/tree/Tree.java
+++ src/main/java/com/tree/Tree.java
@@ -4,9 +4,17 @@ import java.util.List;
 
 public class Tree {
     public String id;
+    public String pid;
     public String name;
     public List<Tree> children;
+    Tree(){
+    }
+
     Tree(String id){
+        this.id=id;
+    }
+    Tree(String pid,String id){
+        this.pid=pid;
         this.id=id;
     }
     Tree(String id,List<Tree> children){
--- src/main/java/com/tree/TreeSimulator.java
+++ src/main/java/com/tree/TreeSimulator.java
@@ -1,8 +1,6 @@
 package com.tree;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class TreeSimulator {
     public static int NUM=10;
@@ -10,7 +8,20 @@ public class TreeSimulator {
        Tree tree= generatorTree();
        System.out.print(tree);
         List<TreeEntity> list= treeToTreeEntity(tree);
-        System.out.print(list);
+      //  System.out.print(list);
+
+        long startT=System.currentTimeMillis();
+        Tree resultTree=treeEntityToTree(list);
+        long endT=System.currentTimeMillis();
+        System.out.println(resultTree);
+        System.out.println("方法1耗时"+(endT-startT));
+        long startT2=System.currentTimeMillis();
+        Tree resultTree2=treeEntityToTree2(list);
+        long endT2=System.currentTimeMillis();
+        System.out.println(resultTree2);
+        System.out.println("方法2耗时"+(endT2-startT2));
+        System.out.println(resultTree);
+        System.out.println(resultTree2);
     }
 
     public static Tree generatorTree(){
@@ -26,7 +37,7 @@ public class TreeSimulator {
         deepth=deepth+1;
         if(deepth<4){
             List<Tree> children=new ArrayList<Tree>();
-            int number=(int)(Math.random()*30);
+            int number=20+(int)(Math.random()*30);
             for(int i=1;i<number;i++){
                 Tree son=new Tree(UUID.randomUUID().toString());
                 generatorTree(son,deepth);
@@ -56,18 +67,109 @@ public class TreeSimulator {
         }
         return list;
     };
+    public static Tree treeEntityToTree(List<TreeEntity> list){
+        Tree tree=new Tree();
+        for(TreeEntity t:list){
+            if(t.pid==null){
+                tree.id=t.id;
+            }
 
+            break;
+        }
+  //      list.remove(t);
+        treeEntityToTree(list,tree);
+        return tree;
+    };
     /**
      * 根据线性列表生成树,ok，重点来了，这是比较麻烦的地方
+     * 最直观的方法,实际运行证明，这个慢的可怕
      * */
-    public static List<TreeEntity> treeEntityToTree(List<TreeEntity> list,Tree tree){
-        if(tree.children!=null){
-            for(Tree t:tree.children){
-                list.add(new TreeEntity(tree.id,t.id));
-                treeToTreeEntity(t,list);
+    public static Tree treeEntityToTree(List<TreeEntity> list,Tree tree){
+        for(TreeEntity t:list){
+            if(t.pid==tree.id){
+                if(tree.children==null){
+                    tree.children=new ArrayList<Tree>();
+                }
+                Tree son=new Tree(t.id);
+                tree.children.add(son);
+                treeEntityToTree(list,son);
             }
         }
-        return list;
+        return tree;
+    };
+
+    public static Tree treeEntityToTree3(List<TreeEntity> list){
+        Tree root=null;
+        List<Tree> treeList=new ArrayList<>();
+        Map<String,Tree> treeMap=new HashMap<>();
+        for(TreeEntity t:list){
+            Tree tree=new Tree(t.pid,t.id);
+            treeMap.put(t.id,tree);
+            treeList.add(tree);
+            if(t.pid==null){
+                root=tree;
+            }
+        }
+
+        Tree tree=new Tree();
+        for(TreeEntity t:list){
+            if(t.pid==null){
+                tree.id=t.id;
+            }
+
+            break;
+        }
+        //      list.remove(t);
+        treeEntityToTree(list,tree);
+        return tree;
+    };
+    /**
+     * 根据线性列表生成树,ok，重点来了，这是比较麻烦的地方
+     * 最直观的方法,实际运行证明，这个慢的可怕
+     * */
+    public static Tree treeEntityToTree3(List<TreeEntity> list,Tree tree){
+        for(TreeEntity t:list){
+            if(t.pid==tree.id){
+                if(tree.children==null){
+                    tree.children=new ArrayList<Tree>();
+                }
+                Tree son=new Tree(t.id);
+                tree.children.add(son);
+                treeEntityToTree(list,son);
+            }
+        }
+        return tree;
+    };
+
+
+
+
+    /**
+     *
+     * */
+    public static Tree treeEntityToTree2(List<TreeEntity> list){
+        Tree root=null;
+        List<Tree> treeList=new ArrayList<>();
+        Map<String,Tree> treeMap=new HashMap<>();
+        for(TreeEntity t:list){
+            Tree tree=new Tree(t.pid,t.id);
+            treeMap.put(t.id,tree);
+            treeList.add(tree);
+            if(t.pid==null){
+                root=tree;
+            }
+        }
+        for(Tree t:treeList){
+            if(t.pid!=null){
+                Tree parent=treeMap.get(t.pid);
+                if(parent.children==null){
+                    parent.children= new ArrayList<Tree>();
+                }
+                parent.children.add(t);
+            }
+
+        }
+        return root;
     };
 
     /**
